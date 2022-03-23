ade5f1774388798cc1eb711c10263161c6e00a88
Web Dev <WebDev@hhhhhhhhh.local>
2022-03-23 16:36:48 +0300

    FIXED debug.js

--- debug.js
+++ debug.js
@@ -10,11 +10,12 @@
  */
 function generateIntegersArray(firstNumber, lastNumber) {
   const integersArray = [];
-  whille (firstNumber < lastNumber)
+  while (firstNumber <= lastNumber) {
     integersArray.push(firstNumber);
-    lastNumber++;
+    firstNumber++;
+  }
 
-  return IntegersArray;
+  return integersArray;
 }
 
 // console.log(generateIntegersArray(3, 7));
@@ -31,17 +32,17 @@ function generateIntegersArray(firstNumber, lastNumber) {
  *
  */
 function noZeroes(numberString) {
-  whil (numberString.startsWith("0")) {
-    numberString.slice(0, -1);
+  while (numberString.startsWith('0')) {
+    numberString = numberString.slice(1);
   }
 
-  while (numberString.endswith("0") {
-    numberString.slice[1];
+  while (numberString.endsWith('0')) {
+    numberString = numberString.slice(0, -1);
   }
-  numberString;
+  return numberString;
 }
 
-// console.log(noZeroes("001100"));
+console.log(noZeroes('001100'));
 
 module.exports = {
   generateIntegersArray,
